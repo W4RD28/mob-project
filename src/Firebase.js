bf2c7c270d8fe677ba160620ad2c22cf80a72289
W4RD28 <windudr@gmail.com>
2021-11-15 20:08:21 +0700

    rollback mistakes

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,4 +1,7 @@
 import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBfjQTGTUBMqgNZmmeadnouWCWzbhdqRT4",
