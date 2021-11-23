a78227c5797d774fbd26299359c35f1b9478b56c
W4RD28 <windudr@gmail.com>
2021-11-23 21:30:28 +0700

    added login redirect

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,20 +2,24 @@ import LoginForm from './LoginForm';
 import '../Styles/Login.css';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../Firebase';
-
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
-    const handleSubmit = data => {
+    let history = useHistory();
+
+    const handleSubmit = data => {    
         signInWithEmailAndPassword(auth, data.email, data.password)
             .then((userCredential) => {
                 const user = userCredential.user;
-                console.log(user);
             })
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
                 console.log(errorCode, errorMessage);
-  });
+            });
+            auth.onAuthStateChanged(user => {
+                history.push('/workspaces');
+            })
     };
 
     return ( 
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -2,21 +2,28 @@ import SignUpForm from "./SignUpForm";
 import '../Styles/SignUp.css';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../Firebase'
+import { useHistory } from 'react-router-dom';
 
-const handleSubmit = data => {
-    createUserWithEmailAndPassword(auth, data.email, data.password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            console.log(user);
-        }) 
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.errorMessage;
-            console.log(errorCode, errorMessage);
-        })
-};
+
 
 const SignUp = () => {
+    let history = useHistory();
+
+    const handleSubmit = data => {
+        createUserWithEmailAndPassword(auth, data.email, data.password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+            }) 
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.errorMessage;
+                console.log(errorCode, errorMessage);
+            })
+            auth.onAuthStateChanged(user => {
+                history.push('/workspaces');
+            })
+    };
+
     return ( 
         <SignUpForm onSubmit={handleSubmit} />
      );
