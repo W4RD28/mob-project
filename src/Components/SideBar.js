00a35789535596c812a5d46006703c84056a6083
Anki Prawira <ankiprawira@gmail.com>
2021-12-08 21:32:37 +0700

    update workspace

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -2,39 +2,51 @@ import '../Styles/SideBar.css'
 import { BellIcon, CogIcon } from '@heroicons/react/outline'
 
 const SideBar = () => {
-    return ( 
+    return (
         <div className="inline-block">
             <nav className="Profile">
                 {/* cari gambar yang kotak nanti lonjong kalo gak bisa */}
-                <img src="https://lh3.googleusercontent.com/a-/AOh14GiXK3mJXWaKrJhb6xBuWP_LjIv0hDWcKGaDVhdDLQ=s288-p-rw-no" 
-                alt="Placeholder User" className="Userimg"/>
+                <a href="#" className="w-12">
+                    <img src="https://lh3.googleusercontent.com/a-/AOh14GiXK3mJXWaKrJhb6xBuWP_LjIv0hDWcKGaDVhdDLQ=s288-p-rw-no"
+                        alt="Placeholder User" className="Userimg" />
+                </a>
 
                 <div className="Icons">
-                    <BellIcon />
-                    <CogIcon />
+                    <a href="#" className="w-12">
+                        <BellIcon />
+                    </a>
+                    <a href="#" className="w-12">
+                        <CogIcon />
+                    </a>
                 </div>
             </nav>
             <nav className="MyWorkspaces">
-                <div className="WorkspaceLink">
-                    <img src="https://lh3.googleusercontent.com/a-/AOh14GiXK3mJXWaKrJhb6xBuWP_LjIv0hDWcKGaDVhdDLQ=s288-p-rw-no" 
-                    alt="Placeholder Group" className="Groupimg" />
-                    <p className="Groupname">Persatuan Komodo</p>
-                </div>
-                <div className="WorkspaceLink">
-                    <img src="https://lh3.googleusercontent.com/a-/AOh14GiXK3mJXWaKrJhb6xBuWP_LjIv0hDWcKGaDVhdDLQ=s288-p-rw-no" 
-                    alt="Placeholder Group" className="Groupimg" />
-                    <p className="Groupname">Komodo Dragon Forum Group</p>
-                </div>
-                <div className="WorkspaceLink">
-                    <img src="https://lh3.googleusercontent.com/a-/AOh14GiXK3mJXWaKrJhb6xBuWP_LjIv0hDWcKGaDVhdDLQ=s288-p-rw-no" 
-                    alt="Placeholder Group" className="Groupimg" />
-                    <p className="Groupname">Keluarga Komodo</p>
-                </div>
+                <button href="#" className="focus:bg-brown focus:bg-opacity-50 rounded-xl">
+                    <div className="WorkspaceLink flex flex-nowrap content-center">
+                        <img src="https://lh3.googleusercontent.com/a-/AOh14GiXK3mJXWaKrJhb6xBuWP_LjIv0hDWcKGaDVhdDLQ=s288-p-rw-no"
+                            alt="Placeholder Group" className="Groupimg" />
+                        <p className="Groupname">Persatuan Komodo</p>
+                    </div>
+                </button>
+                <button className="focus:bg-brown focus:bg-opacity-50 rounded-xl">
+                    <div className="WorkspaceLink flex flex-nowrap content-center">
+                        <img src="https://lh3.googleusercontent.com/a-/AOh14GiXK3mJXWaKrJhb6xBuWP_LjIv0hDWcKGaDVhdDLQ=s288-p-rw-no"
+                            alt="Placeholder Group" className="Groupimg" />
+                        <p className="Groupname">Komodo Dragon Forum Group</p>
+                    </div>
+                </button>
+                <button className="focus:bg-brown focus:bg-opacity-50 rounded-xl">
+                    <div className="WorkspaceLink flex flex-nowrap content-center">
+                        <img src="https://lh3.googleusercontent.com/a-/AOh14GiXK3mJXWaKrJhb6xBuWP_LjIv0hDWcKGaDVhdDLQ=s288-p-rw-no"
+                            alt="Placeholder Group" className="Groupimg" />
+                        <p className="Groupname">Keluarga Komodo</p>
+                    </div>
+                </button>
                 <div className></div>
             </nav>
         </div>
-        
-     );
+
+    );
 }
- 
+
 export default SideBar;
